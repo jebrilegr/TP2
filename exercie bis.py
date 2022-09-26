2b59c7e43a8d0b372f21e427f0650c0bebf3b4d8
jebrillgr <jebril.legarr@gmail.com>
2022-09-26 23:38:44 +0200

    REFACTORING - Format fichier

--- exercie bis.py
+++ exercie bis.py
@@ -4,23 +4,22 @@ annee = int(input("saisir l'annee : "))
 
 print(f"La date choisie est {jour}/{mois}/{annee}")
 
-if(annee%4==0 and annee%100!=0 or annee%400==0):
+if annee % 4 == 0 and annee % 100 != 0 or annee % 400 == 0:
     print(f"L'année {annee} est bissextile. ")
-    isbissxtile=True
+    isbissxtile = True
 else:
     print(f"L'année {annee} n'est pas bissextile. ")
-    isbissxtile=False
+    isbissxtile = False
 
-if not(1 <= mois <= 12):
+if not (1 <= mois <= 12):
     print(f"Numéro de mois incorrect : {mois}")
 
-if(mois==2):
-    if(isbissxtile):
-        nbjours= 29
+if (mois == 2):
+    if (isbissxtile):
+        nbjours = 29
     else:
-        nbjours= 28
-if(mois==1 or mois==3 or mois==5 or mois==7 or mois==8 or mois==10 or mois==12):
-    nbjours=31
-if(mois==4 or mois==6 or mois==9 or mois==11 ):
-    nbjours=30
-
+        nbjours = 28
+if (mois == 1 or mois == 3 or mois == 5 or mois == 7 or mois == 8 or mois == 10 or mois == 12):
+    nbjours = 31
+if (mois == 4 or mois == 6 or mois == 9 or mois == 11):
+    nbjours = 30
