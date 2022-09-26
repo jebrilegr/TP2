28f85813b0c42a03e06355a39e8773ce649b43ba
jebrillgr <jebril.legarr@gmail.com>
2022-09-26 23:16:24 +0200

    Test année bissextile

--- exercie bis.py
+++ exercie bis.py
@@ -3,3 +3,11 @@ mois = int(input("saisir le mois : "))
 annee = int(input("saisir l'annee : "))
 
 print(f"La date choisie est {jour}/{mois}/{annee}")
+
+if(annee%4==0 and annee%100!=0 or annee%400==0):
+    print(f"L'année {annee} est bissextile. ")
+    isbissxtile=True
+else :
+    print(f"L'année {annee} n'est pas bissextile. ")
+    isbissxtile=False
+
